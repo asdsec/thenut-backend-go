0d3621c6dee5f0b9c6408a8004b61e3180d42202
sametdmr <sametdemir1049@gmail.com>
2023-03-09 23:21:17 +0300

    cÄustomer test added

--- db/sqlc/customer_test.go
+++ db/sqlc/customer_test.go
@@ -2,20 +2,80 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
+	"github.com/sametdmr/thenut/utils"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateCustomer(t *testing.T) {
-	user := createRandomUser(t)
+func createRandomCustomer(t *testing.T) Customer {
+	tUser := createRandomUser(t)
 
-	customer, err := testQueries.CreateCustomer(context.Background(), user.Username)
+	customer, err := testQueries.CreateCustomer(context.Background(), tUser.Username)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, customer)
-	require.Equal(t, user.Username, customer.Owner)
+	require.Equal(t, tUser.Username, customer.Owner)
 	require.NotZero(t, customer.CreatedAt)
 	require.NotZero(t, customer.ID)
 	require.NotZero(t, customer.ImageUrl)
+
+	return customer
+}
+
+func TestCreateCustomer(t *testing.T) {
+	tUser := createRandomUser(t)
+
+	customer, err := testQueries.CreateCustomer(context.Background(), tUser.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, customer)
+	require.Equal(t, tUser.Username, customer.Owner)
+	require.NotZero(t, customer.CreatedAt)
+	require.NotZero(t, customer.ID)
+	require.NotZero(t, customer.ImageUrl)
+}
+
+func TestDeleteCustomer(t *testing.T) {
+	tCustomer := createRandomCustomer(t)
+
+	err := testQueries.DeleteCustomer(context.Background(), tCustomer.ID)
+
+	require.NoError(t, err)
+	customer, err := testQueries.GetCustomer(context.Background(), tCustomer.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, customer)
+}
+
+func TestGetCustomer(t *testing.T) {
+	tCustomer := createRandomCustomer(t)
+
+	customer, err := testQueries.GetCustomer(context.Background(), tCustomer.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, customer)
+	require.Equal(t, tCustomer.ID, customer.ID)
+	require.Equal(t, tCustomer.ImageUrl, customer.ImageUrl)
+	require.Equal(t, tCustomer.Owner, customer.Owner)
+	require.WithinDuration(t, tCustomer.CreatedAt, customer.CreatedAt, time.Second)
+}
+
+func TestUpdateCustomer(t *testing.T) {
+	tCustomer := createRandomCustomer(t)
+	arg := UpdateCustomerParams{
+		ID:       tCustomer.ID,
+		ImageUrl: utils.RandomImageUrl(),
+	}
+
+	customer, err := testQueries.UpdateCustomer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, customer)
+	require.Equal(t, arg.ImageUrl, customer.ImageUrl)
+	require.Equal(t, tCustomer.ID, customer.ID)
+	require.Equal(t, tCustomer.Owner, customer.Owner)
+	require.WithinDuration(t, tCustomer.CreatedAt, customer.CreatedAt, time.Second)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/lib/pq v1.10.7
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.2
+	golang.org/x/crypto v0.7.0
 )
 
 require (
@@ -21,7 +22,6 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
